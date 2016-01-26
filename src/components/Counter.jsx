cefd800850523bfc6bb9e2f7ce55c746769f41e5
Jonathan <sunyang713@hotmail.com>
2016-01-26 02:02:26 -0500

    Style counter component.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 export default (props) => (
-  <div>
+  <div style={ { border: '1px solid black', width: 200 } }>
+    <h3>{ 'Counter' }</h3>
     <p>{ 'Current value: ' + props.value }</p>
     <button onClick={ props.handleIncrement }>{ 'Increment' }</button>
     <button onClick={ props.handleDecrement }>{ 'Decrement' }</button>
