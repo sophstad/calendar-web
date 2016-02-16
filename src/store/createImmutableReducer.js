425e403c13eb4e1198135f74e3cd808dd58e245b
Jonathan <sunyang713@hotmail.com>
2016-02-16 13:03:48 -0500

    createImmutableReducer method description.

--- src/store/createImmutableReducer.js
+++ src/store/createImmutableReducer.js
@@ -1,5 +1,9 @@
 import { fromJS, toJS } from 'immutable'
 
+/*
+ * Uses <Immutable> type objects to hydrate the state.
+ * Isolates Immutable to the reducers.
+ */
 export default function createImmutableReducer(initialState, handlers) {
   return (state = initialState, action) => (
     handlers.hasOwnProperty(action.type) ?
