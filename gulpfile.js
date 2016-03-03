98db463dcdcb93dbd4d3007909b2826bd83ed5d0
Jonathan <sunyang713@hotmail.com>
2016-03-03 02:03:40 -0500

    Gulpfile nothing.

--- gulpfile.js
+++ gulpfile.js
@@ -85,11 +85,11 @@ gulp.task("webpack-dev-server", function(callback) {
    */
   app.listen(8080, "localhost", function(err) {
     if (err) throw new gutil.PluginError("webpack-dev-server", err);
-    // Server listening
-    gutil.log("[webpack-dev-server]", "==> 🌎  Listening on port 8080.", "Open up http://localhost:8080/ in your browser.");
-
-    // keep the server alive or continue?
-    // callback();
+    gutil.log(
+      "[webpack-dev-server]",
+      "==> 🌎  Listening on port 8080.",
+      "Open up http://localhost:8080/ in your browser."
+    );
   });
 });
 
