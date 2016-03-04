3cee1a25c644d517b188e41b19d3fbe69de94a18
Jonathan <sunyang713@hotmail.com>
2016-03-04 01:03:15 -0500

    Move 'Routes' to root-level.

--- src/containers/Root/Root.prod.jsx
+++ src/containers/Root/Root.prod.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import configureStore from 'store/configureStore'
-import Routes from './Routes'
+import Routes from 'Routes'
 
 const initialState = window.__INITIAL_STATE__
 const store = configureStore(initialState)
