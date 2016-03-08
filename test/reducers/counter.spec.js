f7256d0c8185de86296f55242126bb013db21905
Jonathan <sunyang713@hotmail.com>
2016-03-08 18:08:19 -0500

    Inital tests for counter module.

---
+++ test/reducers/counter.spec.js
@@ -0,0 +1,19 @@
+import test from 'ava'
+import reducerTest from 'immutableReducerTest'
+import counterReducer from 'reducers/counter.js'
+import * as counterActions from 'actions/counter.js'
+const num = 4
+
+test('handle increment', reducerTest(
+  counterReducer,
+  { value: num },
+  counterActions.increment(),
+  { value: num + 1 }
+))
+
+test('handle decrement', reducerTest(
+  counterReducer,
+  { value: num },
+  counterActions.decrement(),
+  { value: num - 1 }
+))
